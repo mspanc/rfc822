7a68eda0fcbb50fe686a80c8bd6878271423bb80
Marcin Lewandowski <marcin@saepia.net>
2011-11-21 18:05:25 +0100

    Init

---
+++ Rakefile
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+
+require 'jeweler'
+Jeweler::Tasks.new do |s|
+  s.name = "rfc822"
+  s.version = '0.1.0'
+  s.authors = ["Marcin Lewandowski"]
+  s.email = ["edek123456@gmail.com"]
+  s.summary = %q{Gem containing just e-mail validation regexp based on RFC822, please use RFC822::EMAIL_REGEXP_WHOLE for checking if whole string is an valid email, and RFC822::EMAIL_REGEXP_PART for e.g. scanning a string for e-mail address extraction.}
+  s.description = %q{Gem containing just e-mail validation regexp based on RFC822, please use RFC822::EMAIL_REGEXP_WHOLE for checking if whole string is an valid email, and RFC822::EMAIL_REGEXP_PART for e.g. scanning a string for e-mail address extraction.}
+  s.homepage = "http://github.com/saepia/rfc822"
+  s.files = ["Gemfile", "rfc822.gemspec", "lib/", "lib/rfc822.rb"]
+  s.require_paths = ["lib"]
+
+end
+Jeweler::RubygemsDotOrgTasks.new
+
---
+++ lib/rfc822.rb
@@ -0,0 +1,44 @@
+module RFC822
+  QTEXT = '[^\\x0d\\x22\\x5c\\x80-\\xff]'
+  DTEXT = '[^\\x0d\\x5b-\\x5d\\x80-\\xff]'
+  ATOM = '[^\\x00-\\x20\\x22\\x28\\x29\\x2c\\x2e\\x3a-' +
+    '\\x3c\\x3e\\x40\\x5b-\\x5d\\x7f-\\xff]+'
+  QUOTED_PAIR = '\\x5c[\\x00-\\x7f]'
+  DOMAIN_LITERAL = "\\x5b(?:#{DTEXT}|#{QUOTED_PAIR})*\\x5d"
+  QUOTED_STRING = "\\x22(?:#{QTEXT}|#{QUOTED_PAIR})*\\x22"
+  DOMAIN_REF = ATOM
+  SUB_DOMAIN = "(?:#{DOMAIN_REF}|#{DOMAIN_LITERAL})"
+  WORD = "(?:#{ATOM}|#{QUOTED_STRING})"
+  DOMAIN = "#{SUB_DOMAIN}(?:\\x2e#{SUB_DOMAIN})*"
+  LOCAL_PART = "#{WORD}(?:\\x2e#{WORD})*"
+  ADDR_SPEC = "#{LOCAL_PART}\\x40#{DOMAIN}"
+
+  EMAIL_REGEXP_WHOLE = Regexp.new("^#{ADDR_SPEC}$", nil, 'n')
+  EMAIL_REGEXP_PART = Regexp.new("#{ADDR_SPEC}", nil, 'n')
+end
+
+class String
+  def is_email?
+    if RUBY_VERSION >= "1.9"
+      (self.force_encoding("BINARY") =~ RFC822::EMAIL_REGEXP_WHOLE) != nil
+    else
+      (self =~ RFC822::EMAIL_REGEXP_WHOLE) != nil
+    end
+  end
+
+  def contains_email?
+    if RUBY_VERSION >= "1.9"
+      (self.force_encoding("BINARY") =~ RFC822::EMAIL_REGEXP_PART) != nil
+    else
+      (self =~ RFC822::EMAIL_REGEXP_PART) != nil
+    end
+  end
+
+  def scan_for_emails
+    if RUBY_VERSION >= "1.9"
+      self.force_encoding("BINARY").scan(RFC822::EMAIL_REGEXP_PART)
+    else
+      self.scan(RFC822::EMAIL_REGEXP_PART)
+    end
+  end
+end
